fa7ecb60d1ad968ce154c532d5db7cd6af17c9cc
isandeepmakwana <isandeepmakwana1@gmail.com>
2020-07-10 18:19:45 +0530

    re start app

--- insta_app/insta_app/settings.py
+++ insta_app/insta_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lbs9q9)r_^zy5txa^hn8eripalp1!uq*yhlkt==xxz!kfnj*o^'
+SECRET_KEY = '&lbis!ao8-)_x9ll#$wh1ao0j6bry3$lyhhn0u#nx-b7#r4(ge'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
